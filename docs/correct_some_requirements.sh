2348cee790021d20540995386c5556bdc21085af
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-10-26 16:13:04 +0200

    fix: fix pin pyyaml -3

--- docs/correct_some_requirements.sh
+++ docs/correct_some_requirements.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep -rl 'opentelemetry-exporter-prometheus' extra-requirements.txt | xargs sed -i 's/opentelemetry-exporter-prometheus==1.12.0/opentelemetry-exporter-prometheus>=1.12.0/g'
+grep -rl 'opentelemetry-exporter-prometheus' extra-requirements.txt | xargs sed -i 's/opentelemetry-exporter-prometheus==1.12.0/opentelemetry-exporter-prometheus==1.12.0rc1/g'
 grep -rl 'pyyaml' extra-requirements.txt | xargs sed -i 's/pyyaml==5.3.1/pyyaml==5.4.1/g'
